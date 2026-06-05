aea02b8884852be7f0a6a98c18d48f23808f8ca8
FranLopez <fl06francisco@gmail.com>
2026-06-05 18:50:51 -0300

    Agregamos componentes Header, Main, Footer

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer>
+      <section>
+        <p>© 2026 Flowness — Todos los derechos reservados</p>
+        <p>Hipólito Irigoyen 383, Tinogasta, Catamarca</p>
+      </section>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/layout/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="/">Inicio</a></li>
+          <li><a href="/metodo">El Método</a></li>
+          <li><a href="/cursos">Cursos</a></li>
+          <li><a href="/clases">Clases Online</a></li>
+          <li><a href="/contacto">Contacto</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
